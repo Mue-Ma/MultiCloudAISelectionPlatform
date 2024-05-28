b85de9b2e63baa7ab1b01a7f910d3e5595cb4e6e
Christina Niederer <christina.niederer@slidelizard.at>
2024-05-28 10:19:00 +0200

    add metrics weight selectors

--- src/MultiCloudAISelectionPlatform/MultiCloudAISelectionPlatform.Common/Models/MetrikWeights.cs
+++ src/MultiCloudAISelectionPlatform/MultiCloudAISelectionPlatform.Common/Models/MetrikWeights.cs
@@ -2,8 +2,8 @@
 {
     public class MetrikWeights
     {
-        public int Accuracy { get; set; } = 1;
-        public int Costs { get; set; } = 1;
-        public int ResponseTime { get; set; } = 1;
+        public double Accuracy { get; set; } = 1.0;
+        public double Costs { get; set; } = 1.0;
+        public double ResponseTime { get; set; } = 1.0;
     }
 }
